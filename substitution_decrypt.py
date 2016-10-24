9583ac386789e6bd3367d6012474d79025b4279d
Tom Oberhauser <tom@devfoo.de>
2016-10-24 16:06:13 +0200

    Stage 2 - short words

--- substitution_decrypt.py
+++ substitution_decrypt.py
@@ -79,8 +79,6 @@ def display_fancy(name, text, key=None):
     print('{:*^100}'.format(''))
 
 
-
-
 def stage1(cyphertext):
     """
     Searches for most common characters and replaces them with the most common characters of the english language.
@@ -88,17 +86,16 @@ def stage1(cyphertext):
     :param c: cyphertext after stage 1
     :return: new cyphertext, sub-key
     """
-    MOST_COMMON_CHARS_ENG = ' et'
-    # MOST_COMMON_CHARS_ENG = ' etaoin'
     # MOST_COMMON_CHARS_ENG = ' etaoinshrdlu'
+    MOST_COMMON_CHARS_ENG = ' et'
     char_counter = defaultdict(int)
     for char in cyphertext:
         char_counter[char] += 1
     MOST_COMMON_CHARS_DOC = ''.join([x[0] for x in sorted(char_counter.items(), key=lambda x: x[1], reverse=True)])
     sub_key = {}
     print(sorted(char_counter.items(), key=lambda x: x[1], reverse=True))
-    for x, X in zip(MOST_COMMON_CHARS_DOC, MOST_COMMON_CHARS_ENG):
-        sub_key[x] = X
+    for x, Y in zip(MOST_COMMON_CHARS_DOC, MOST_COMMON_CHARS_ENG):
+        sub_key[x] = Y
     return apply_substitution_dictionary(cyphertext, sub_key), sub_key
 
 
@@ -108,10 +105,16 @@ def stage2(cyphertext):
     :param cyphertext: after stage 2
     :return: new cyphertext, sub-key
     """
+    # MOST_COMMON_ONE_CHAR_WORDS_ENG = ['a', 'i']
+    # MOST_COMMON_TWO_CHAR_WORDS_ENG = ['of', 'to', 'in']
+    # MOST_COMMON_THREE_CHAR_WORDS_ENG = ['the', 'and', 'for']
+    MOST_COMMON_ONE_CHAR_WORDS_ENG = ['a']
+    MOST_COMMON_TWO_CHAR_WORDS_ENG = ['of']
+    MOST_COMMON_THREE_CHAR_WORDS_ENG = ['the']
     one_char_word_counter = defaultdict(int)
     two_char_word_counter = defaultdict(int)
     three_char_word_counter = defaultdict(int)
-
+    sub_key = {}
     for token in cyphertext.split():
         if len(token) == 1:
             one_char_word_counter[token] += 1
@@ -119,11 +122,19 @@ def stage2(cyphertext):
             two_char_word_counter[token] += 1
         if len(token) == 3:
             three_char_word_counter[token] += 1
-    print(sorted(one_char_word_counter.items(), key=lambda x: x[1], reverse=True))
-    print(sorted(two_char_word_counter.items(), key=lambda x: x[1], reverse=True))
-    print(sorted(three_char_word_counter.items(), key=lambda x: x[1], reverse=True))
-
-    return 'nothing', 'nothing'
+    MOST_COMMON_ONE_CHAR_WORDS_DOC = [x[0] for x in
+                                      sorted(one_char_word_counter.items(), key=lambda x: x[1], reverse=True)]
+    MOST_COMMON_TWO_CHAR_WORDS_DOC = [x[0] for x in
+                                      sorted(two_char_word_counter.items(), key=lambda x: x[1], reverse=True)]
+    MOST_COMMON_THREE_CHAR_WORDS_DOC = [x[0] for x in
+                                        sorted(three_char_word_counter.items(), key=lambda x: x[1], reverse=True)]
+    for x, Y in zip(MOST_COMMON_ONE_CHAR_WORDS_DOC, MOST_COMMON_ONE_CHAR_WORDS_ENG):
+        sub_key[x] = Y
+    for x, Y in list(zip(MOST_COMMON_TWO_CHAR_WORDS_DOC, MOST_COMMON_TWO_CHAR_WORDS_ENG)) + list(
+            zip(MOST_COMMON_THREE_CHAR_WORDS_DOC, MOST_COMMON_THREE_CHAR_WORDS_ENG)):
+        for x_c, X_c in zip(x, Y):
+            sub_key[x_c] = X_c
+    return apply_substitution_dictionary(cyphertext, sub_key), sub_key
 
 
 ##############################################################################
@@ -133,4 +144,4 @@ display_fancy('INPUT', cyphertext)
 cypher_stage1, key_stage_1 = stage1(cyphertext)
 display_fancy('STAGE 1 - CHARACTER STATS', cypher_stage1)
 cypher_stage2, key_stage_2 = stage2(cypher_stage1)
-display_fancy('STAGE 2 - BIGRAM STATISTICS', cypher_stage2)
+display_fancy('STAGE 2 - SHORT WORDS STATISTICS', cypher_stage2)
