1e611290e961767541be2226596bfc088bf6a0b1
Tom Oberhauser <tom@devfoo.de>
2016-10-26 01:56:49 +0200

    Added small dictionary

---
+++ playground/wordlist.py
@@ -0,0 +1,12 @@
+words = []
+
+for word in open('textfiles/short_words.txt'):
+    word = word.rstrip()
+    if len(word) > 3:
+        words.append(word.rstrip())
+
+for word in open('textfiles/medium_words.txt'):
+    words.append(word.rstrip())
+
+
+print(words)
